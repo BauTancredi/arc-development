92a044afd3f76ced3d364fb09544db276e4875c2
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-09-22 14:47:39 -0300

    Active tab refresh fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import AppBar from "@material-ui/core/AppBar";
@@ -30,7 +30,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "3em",
   },
   logo: {
-    height: "7em",
+    height: "8em",
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
   },
   tabContainer: {
     marginLeft: "auto",
@@ -53,6 +59,22 @@ const Header = (props) => {
   const classes = useStyles();
   const [value, setValue] = useState(0);
 
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   const handleChange = (e, value) => {
     setValue(value);
   };
@@ -62,7 +84,15 @@ const Header = (props) => {
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
-            <img src={logo} alt="Company logo" className={classes.logo} />
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+              disableRipple
+            >
+              <img src={logo} alt="Company logo" className={classes.logo} />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChange}
