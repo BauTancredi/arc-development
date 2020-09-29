fef10769ad088a50a7ad40d88b40b7eea0ec0107
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-09-29 16:53:07 -0300

    Creating and styling the information block

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -49,6 +49,11 @@ export default createMuiTheme({
       fontSize: "1.25rem",
       color: arcGrey,
     },
+    subtitle2: {
+      fontWeight: 300,
+      fontSize: "1.25rem",
+      color: "white",
+    },
     learnButton: {
       borderColor: arcBlue,
       color: arcBlue,
